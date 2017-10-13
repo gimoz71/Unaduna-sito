780de9e4990905247c1733fc14acd8d34745d375
Gianluca Monti <gm3871@gmail.com>
2017-10-13 11:06:52 +0200

    wip elementi js configuratore

--- javascripts/app/controllers/componentsController.js
+++ javascripts/app/controllers/componentsController.js
@@ -1,12 +1,12 @@
 angular.module("applicationModule").controller("componentsController", function($scope) {
-  
+
 	$scope.isHome = true;
 	$scope.isConfigurator = false;
 	$scope.isVisione = false;
 	$scope.isEsperienza = false;
 	$scope.isContatti = false;
 	$scope.isStores = false;
-	
+
 	$scope.wowInit = function(config){
 		if(config){
 			new WOW(config).init();
@@ -14,7 +14,7 @@ angular.module("applicationModule").controller("componentsController", function(
 			new WOW().init();
 		}
 	}
-	
+
 	$scope.setHome = function(){
 		$scope.isHome = true;
 		$scope.isConfigurator = false;
@@ -23,7 +23,7 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = false;
 		$scope.isStores = false;
 	};
-	
+
 	$scope.setConfigurator = function(){
 		$scope.isHome = false;
 		$scope.isConfigurator = true;
@@ -32,7 +32,7 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = false;
 		$scope.isStores = false;
 	};
-	
+
 	$scope.setVisione = function(){
 		$scope.isHome = false;
 		$scope.isConfigurator = false;
@@ -41,7 +41,7 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = false;
 		$scope.isStores = false;
 	};
-	
+
 	$scope.setEsperienza = function(){
 		$scope.isHome = false;
 		$scope.isConfigurator = false;
@@ -50,7 +50,7 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = false;
 		$scope.isStores = false;
 	};
-	
+
 	$scope.setContatti = function(){
 		$scope.isHome = false;
 		$scope.isConfigurator = false;
@@ -59,7 +59,7 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = true;
 		$scope.isStores = false;
 	};
-	
+
 	$scope.setStores = function(){
 		$scope.isHome = false;
 		$scope.isConfigurator = false;
@@ -68,8 +68,69 @@ angular.module("applicationModule").controller("componentsController", function(
 		$scope.isContatti = false;
 		$scope.isStores = true;
 	};
-	
+
 	$scope.initConfiguratore = function(){
+        /*------------------------------------*/
+        /* settaggio  vari swiper */
+        /*------------------------------------*/
+
+        var swiper = new Swiper('.accessori-categoria', {
+            pagination: '.swiper-pagination',
+            slidesPerView: 6,
+            paginationClickable: true,
+            spaceBetween: 0,
+            keyboardControl: true,
+            grabCursor: true,
+            breakpoints: {
+               320: {
+                   slidesPerView: 3
+               },
+               480: {
+                   slidesPerView: 3
+               },
+               768: {
+                   slidesPerView: 4
+               },
+               992: {
+                   slidesPerView: 6
+               },
+               1200: {
+                   slidesPerView: 6
+               }
+           }
+        });
+        var swiper = new Swiper('.accessori-thumb', {
+            pagination: '.swiper-pagination',
+            slidesPerView: 8,
+            paginationClickable: true,
+            spaceBetween: 0,
+            keyboardControl: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            // centeredSlides: 'true',
+            keyboardControl: 'true',
+            grabCursor: true,
+            lazyLoading: 'true',
+            // slidesOffsetBefore: 60,
+            // slidesOffsetAfter: 60,
+            breakpoints: {
+               320: {
+                   slidesPerView: 1
+               },
+               480: {
+                   slidesPerView: 3
+               },
+               768: {
+                   slidesPerView: 5
+               },
+               992: {
+                   slidesPerView: 5
+               },
+               1200: {
+                   slidesPerView: 8
+               }
+           }
+        });
 		$('#a-middle').centerElement();
 	    $.fn.sepLine('first-divider', 'swiper-container', 'accessori');
 	    $.fn.yammHeight('navbar-nav', 'yamm-content','riepilogo')
@@ -83,5 +144,5 @@ angular.module("applicationModule").controller("componentsController", function(
 	        // })
 	    });
 	};
-	
+
 })
